4ab1dcdeb8af704d5aa0dcfe268e1fb9b2bcbdd1
samuelsukanda <samuelsukanda24@gmail.com>
2022-09-30 16:15:22 +0700

    web-bookayo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -13,7 +27,7 @@ function scrollActive(){
         }else{
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
-    })
+    }) 
 }
 window.addEventListener('scroll', scrollActive)
 
@@ -32,3 +46,5 @@ function scrollTop(){
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
+
+console.log();
